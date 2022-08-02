8ff3670ba24e291730f2be9cb23706a88b38d741
nverhulp <105401594+nverhulp@users.noreply.github.com>
2022-08-01 20:03:22 -0700

    updated and cleaned up code

--- assets/script.js
+++ assets/script.js
@@ -1,81 +1,86 @@
 // Created Variable IDs
 var containerQuestionEl = document.getElementById("question-container");
 var containerStartEl = document.getElementById("starter-container");
-var containerEndEl = document.getElementById("end-container");
-var containerScoreEl = document.getElementById("score-banner");
-var formInitials = document.getElementById("initials-form");
-var containerHighScoresEl = document.getElementById("high-score-container");
-var viewHighScoreEl = document.getElementById("view-high-scores");
-var listHighSccoreEl = document.getElementById("high-score-list");
-var correctEl = document.getElementById("correct");
-var wrongEl = document.getElementById("wrong");
+var containerEndEl = document.getElementById("end-container")
+var containerScoreEl = document.getElementById("score-banner")
+var formInitials = document.getElementById("initials-form")
+var containerHighScoresEl = document.getElementById("high-score-container")
+var ViewHighScoreEl = document.getElementById("view-high-scores")
+var listHighScoreEl = document.getElementById("high-score-list")
+var correctEl = document.getElementById("correct")
+var wrongEl = document.getElementById("wrong")
 
 // Button Variables
-var buttonStartEl = document.querySelector("start-game");
-var buttonGoBackEl = document.querySelector("go-back");
-var buttonClearScoresEl = document.querySelector("clear-high-scores");
+var btnStartEl = document.querySelector("#start-game");
+var btnGoBackEl = document.querySelector("#go-back")
+var btnClearScoresEl = document.querySelector("#clear-high-scores")
 
 // Questions and Answers 
-var questionEl = document.getElementById("question");
-var answerButtonsEl = document.getElementById("answer-buttons");
+var questionEl = document.getElementById("question")
+var answerbuttonsEl = document.getElementById("answer-buttons")
 
 // Timer and Score
 var timerEl = document.querySelector("#timer");
 var score = 0;
 var timeleft;
-var gameover;
+var gameover
 timerEl.innerText = 0;
 
 // Array for High Score
-var highScores = [];
+var HighScores = [];
 
 // Array for Questions
 var arrayShuffledQuestions;
-var questionIndex = 0;
+var QuestionIndex = 0;
 
 // List of Questions for Code Quiz
 var questions = [
-    {q: 'Commonly used data types DO NOT include:',
-     a: '3. Alerts',
-        choices:[{choice: '1. Strings'}, {choice: '2. Booleans'}, {choice: '3. Alerts'}, {choice: '4. Numbers'}]
+    {
+        q: 'Commonly used data types DO NOT include:',
+        a: '3. Alerts',
+        choices: [{ choice: '1. Strings' }, { choice: '2. Booleans' }, { choice: '3. Alerts' }, { choice: '4. Numbers' }]
     },
-    {q: 'The condition in an if / else statement is enclosed with __________.',
-     a: '3. Parenthesis',
-            choices:[{choice: '1. Quotes'}, {choice: '2. Curly Brackets'}, {choice: '3. Parenthesis'}, {choice: '4. Square Brackets'}]
+    {
+        q: 'The condition in an if / else statement is enclosed with __________.',
+        a: '3. Parenthesis',
+        choices: [{ choice: '1. Quotes' }, { choice: '2. Curly Brackets' }, { choice: '3. Parenthesis' }, { choice: '4. Square Brackets' }]
     },
-    {q: 'Arrays in Javascript can be used to store __________.',
-     a: '4. All of the above',
-        choices:[{choice: '1. Numbers'}, {choice: '2. Booleans'}, {choice: '3. Strings'}, {choice: '4. All of the above'}]
+    {
+        q: 'Arrays in Javascript can be used to store __________.',
+        a: '4. All of the above',
+        choices: [{ choice: '1. Numbers' }, { choice: '2. Booleans' }, { choice: '3. Strings' }, { choice: '4. All of the above' }]
     },
-    {q: 'String values must be enclosed within __________ when being assigned to variables.',
-     a: '3. Quotes',
-     choices:[{choice: '1. Commas'}, {choice: '2. Curly Brackets'}, {choice: '3. Quotes'}, {choice: '4. Parenthesis'}]
+    {
+        q: 'String values must be enclosed within __________ when being assigned to variables.',
+        a: '3. Quotes',
+        choices: [{ choice: '1. Commas' }, { choice: '2. Curly Brackets' }, { choice: '3. Quotes' }, { choice: '4. Parenthesis' }]
     },
-    {q: 'A very useful tool used during development and debugging for printing content to the debugger is:',
-     a: '4. Console.log',
-     choices:[{choice: '1. JavaScript'}, {choice: '2. Terminal/Bash'}, {choice: '3. For Loops'}, {choice: '4. Console.log'}]
+    {
+        q: 'A very useful tool used during development and debugging for printing content to the debugger is:',
+        a: '4. Console.log',
+        choices: [{ choice: '1. JavaScript' }, { choice: '2. Terminal/Bash' }, { choice: '3. For Loops' }, { choice: '4. Console.log' }]
     },
 ];
 
 // Back Button on High Score Page
 var renderStartPage = function () {
-    containerHighScoresEl.classList.add("hide");
-    containerHighScoresEl.classList.remove("show");
-    containerStartEl.classList.remove("hide");
-    containerStartEl.classList.add("show");
-    containerScoreEl.removeChild(containerScoreEl.lastChild);
-    questionIndex = 0;
-    gameover = "";
-    timerEl.textContent = 0;
-    score = 0;
+    containerHighScoresEl.classList.add("hide")
+    containerHighScoresEl.classList.remove("show")
+    containerStartEl.classList.remove("hide")
+    containerStartEl.classList.add("show")
+    containerScoreEl.removeChild(containerScoreEl.lastChild)
+    QuestionIndex = 0
+    gameover = ""
+    timerEl.textContent = 0
+    score = 0
 
     if (correctEl.className = "show") {
         correctEl.classList.remove("show");
-        correctEl.classList.add("hide");
+        correctEl.classList.add("hide")
     }
     if (wrongEl.className = "show") {
         wrongEl.classList.remove("show");
-        wrongEl.classList.add("hide)";)
+        wrongEl.classList.add("hide");
     }
 }
 
@@ -83,145 +88,144 @@ var renderStartPage = function () {
 var setTime = function () {
     timeleft = 60;
 
-var timercheck = setInterval(function() {
-    timerEl.innerText = timeleft;
-    timeleft--
+    var timercheck = setInterval(function () {
+        timerEl.innerText = timeleft;
+        timeleft--
 
-    if (gameover) {
-        clearInterval(timercheck);
-    }
+        if (gameover) {
+            clearInterval(timercheck);
+        }
 
-    if (timeleft < 0) {
-        showScore();
-        timerEl.innerText = 0;
-        clearInterval(timercheck);
-    }
+        if (timeleft < 0) {
+            showScore();
+            timerEl.innerText = 0;
+            clearInterval(timercheck);
+        }
 
     }, 1000);
 }
 
-var startGame = function() {
-// Show or hide start screen
+var startGame = function () {
+    // Show or hide start screen
     containerStartEl.classList.add('hide');
     containerStartEl.classList.remove('show');
     containerQuestionEl.classList.remove('hide');
     containerQuestionEl.classList.add('show');
-    arrayShuffledQuestions = questions.sort(() => Math.random() - 0.5);
-    setTime();
-    setQuestion();
+    arrayShuffledQuestions = questions.sort(() => Math.random() - 0.5)
+    setTime()
+    setQuestion()
 }
 
 // Next Question for Quiz
-var setQuestion = function() {
-    resetAnswers();
-    displayQuestion(arrayShuffledQuestions[questionIndex]);
+var setQuestion = function () {
+    resetAnswers()
+    displayQuestion(arrayShuffledQuestions[QuestionIndex])
 }
 
 // Remove Answer Buttons
-var resetAnswers = function() {
-    while (answerButtonsEl.firstChild) {
-        answerButtonsEl.removeChild(answerButtonsEl.firstChild);
+var resetAnswers = function () {
+    while (answerbuttonsEl.firstChild) {
+        answerbuttonsEl.removeChild(answerbuttonsEl.firstChild)
     };
 };
 
 // Questions and Answer Buttons
-var displayQuestion = function(index) {
-    questionEl.innerText = index.q;
-    for (var i = 0; i < index.choice.length; i++) {
-        var answerbutton = document.createElement('button');
-        answerbutton.innerText = index.choices[i].choice;
-        answerbutton.classList.add('button');
-        answerbutton.classList.add('answer-button');
-        answerbutton.addEventListener("click", answerCheck);
-        answerButtonsEl.appendChild(answerbutton)
+var displayQuestion = function (index) {
+    questionEl.innerText = index.q
+    for (var i = 0; i < index.choices.length; i++) {
+        var answerbutton = document.createElement('button')
+        answerbutton.innerText = index.choices[i].choice
+        answerbutton.classList.add('btn')
+        answerbutton.classList.add('answerbtn')
+        answerbutton.addEventListener("click", answerCheck)
+        answerbuttonsEl.appendChild(answerbutton)
     }
 };
 
 // Show that answer is correct on screen
-var answerCorrect = function() {
+var answerCorrect = function () {
     if (correctEl.className = "hide") {
-        correctEl.classList.remove("hide");
-        correctEl.classList.add("banner");
-        wrongEl.classList.remove("banner");
-        wrongEl.classList.add("hide");
+        correctEl.classList.remove("hide")
+        correctEl.classList.add("banner")
+        wrongEl.classList.remove("banner")
+        wrongEl.classList.add("hide")
     }
 }
 
 // Show that answer is wrong on screen
-var answerWrong = function() {
+var answerWrong = function () {
     if (wrongEl.className = "hide") {
-        wrongEl.classList.remove("hide");
-        wrongEl.classList.add("banner");
-        correctEl.classList.remove("banner");
-        correctEl.classList.add("hide");
+        wrongEl.classList.remove("hide")
+        wrongEl.classList.add("banner")
+        correctEl.classList.remove("banner")
+        correctEl.classList.add("hide")
     }
 }
 
 // Check Answer
-var answerCheck = function(event) {
-    var selectedAnswer = event.target
-        if (arrayShuffledQuestions[questionIndex].a === selectedAnswer.innerText){
-            answerCorrect()
-            score = score + 7;
-        }
-        
-        else {
-            answerWrong()
-            score = score - 1;
-            timeleft = timeleft - 3;
-        };
-// Check if there is next question and go to if so
-questionIndex++
-        if (arrayShuffledQuestions.length > questionIndex + 1) {
-            setQuestion();
-        }
+var answerCheck = function (event) {
+    var selectedanswer = event.target
+    if (arrayShuffledQuestions[QuestionIndex].a === selectedanswer.innerText) {
+        answerCorrect()
+        score = score + 7
+    }
 
-        else {
-            gameover = "true";
-            showScore();
-        }
+    else {
+        answerWrong()
+        score = score - 1;
+        timeleft = timeleft - 3;
+    };
+    // Check if there is next question and go to if so
+    QuestionIndex++
+    if (arrayShuffledQuestions.length > QuestionIndex + 1) {
+        setQuestion()
+    }
+    else {
+        gameover = "true";
+        showScore();
+    }
 }
 
 // Score at the end
-var showScore = function() {
+var showScore = function () {
     containerQuestionEl.classList.add("hide");
     containerEndEl.classList.remove("hide");
     containerEndEl.classList.add("show");
 
     var scoreDisplay = document.createElement("p");
-    scoreDisplay.innerText = ("Your final score is" + score + "!");
+    scoreDisplay.innerText = ("Your final score is " + score + "!");
     containerScoreEl.appendChild(scoreDisplay);
 }
 
 // High Score Values
-var createHighScore = function(event) {
+var createHighScore = function (event) {
     event.preventDefault()
     var initials = document.querySelector("#initials").value;
     if (!initials) {
-        alert ("Enter your initials!");
+        alert("Enter your intials!");
         return;
     }
 
     formInitials.reset();
 
-    var highScore = {
-        initials: initials, 
+    var HighScore = {
+        initials: initials,
         score: score
     }
 
     // Sort scores
-    highScores.push(highScore);
-    highScores.sort((a,b) => {return b.score-a.score});
+    HighScores.push(HighScore);
+    HighScores.sort((a, b) => { return b.score - a.score });
 
-    while (listHighSccoreEl.firstChild) {
-        listHighSccoreEl.removeChild(listHighSccoreEl.firstChild);
+    while (listHighScoreEl.firstChild) {
+        listHighScoreEl.removeChild(listHighScoreEl.firstChild)
     }
 
-    for (var i = 0; i < highScores.length; i++) {
-        var highScoreEl = document.createElement("li");
-        highScoreEl.className = "high-score";
-        highScoreEl.innerHTML = highScores[i].initials + " - " + highScores[i].score;
-        listHighSccoreEl.appendChild(highScoreEl);
+    for (var i = 0; i < HighScores.length; i++) {
+        var highscoreEl = document.createElement("li");
+        highscoreEl.ClassName = "high-score";
+        highscoreEl.innerHTML = HighScores[i].initials + " - " + HighScores[i].score;
+        listHighScoreEl.appendChild(highscoreEl);
     }
 
     saveHighScore();
@@ -230,39 +234,42 @@ var createHighScore = function(event) {
 
 // Save and View High Scores
 var saveHighScore = function () {
-    localStorage.setItem("highScores", JSON.stringify(highScores));
+    localStorage.setItem("HighScores", JSON.stringify(HighScores))
+
 }
 
 var loadHighScore = function () {
-    var loadedHighScores = localStorage.getItem("highScores");
-    if (!loadedHighScores) {
+    var LoadedHighScores = localStorage.getItem("HighScores")
+    if (!LoadedHighScores) {
         return false;
     }
 
-    loadedHighScores = JSON.parse(loadedHighScores);
-    loadedHighScores.sort((a,b) => {return b.score-a.score});
+    LoadedHighScores = JSON.parse(LoadedHighScores);
+    LoadedHighScores.sort((a, b) => { return b.score - a.score })
 
-    for (var i = 0; i < loadedHighScores.length; i++) {
-        var highScoreEl = document.createElement("li");
-        highScoreEl.className = "high-score";
-        highScoreEl.innerText = loadedHighScores[i].initials + " - " + loadedHighScores[i].score;
-        listHighSccoreEl.appendChild(highScoreEl);
 
-        highScores.push(loadedHighScores[i]);
+    for (var i = 0; i < LoadedHighScores.length; i++) {
+        var highscoreEl = document.createElement("li");
+        highscoreEl.ClassName = "high-score";
+        highscoreEl.innerText = LoadedHighScores[i].initials + " - " + LoadedHighScores[i].score;
+        listHighScoreEl.appendChild(highscoreEl);
+
+        HighScores.push(LoadedHighScores[i]);
+
     }
 }
 
 // High Score Screen
-var displayHighScores = function() {
+var displayHighScores = function () {
+
     containerHighScoresEl.classList.remove("hide");
     containerHighScoresEl.classList.add("show");
-    gameover = "true";
+    gameover = "true"
 
     if (containerEndEl.className = "show") {
         containerEndEl.classList.remove("show");
         containerEndEl.classList.add("hide");
     }
-
     if (containerStartEl.className = "show") {
         containerStartEl.classList.remove("show");
         containerStartEl.classList.add("hide");
@@ -282,23 +289,26 @@ var displayHighScores = function() {
         wrongEl.classList.remove("show");
         wrongEl.classList.add("hide");
     }
+
 }
 
 // Clear High Scores
-var clearScores = function() {
-    highScores = [];
+var clearScores = function () {
+    HighScores = [];
 
-    while(listHighSccoreEl.firstChild) {
-        listHighSccoreEl.removeChild(listHighSccoreEl.firstChild);
+    while (listHighScoreEl.firstChild) {
+        listHighScoreEl.removeChild(listHighScoreEl.firstChild);
     }
 
-    localStorage.clear(highScores);
+    localStorage.clear(HighScores);
+
 }
+
 
 loadHighScore()
 
 // Buttons and Forms
-buttonStartEl.addEventListener("click",startGame);
+buttonStartEl.addEventListener("click", startGame);
 formInitials.addEventListener("submit", createHighScore);
 viewHighScoreEl.addEventListener("click", displayHighScores);
 buttonGoBackEl.addEventListener("click", renderStartPage);
