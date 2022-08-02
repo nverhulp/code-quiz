cc0bf7510676621af2dfb38a94f42a887552752a
nverhulp <105401594+nverhulp@users.noreply.github.com>
2022-08-01 17:20:42 -0700

    added questions in js file

--- assets/script.js
+++ assets/script.js
@@ -34,4 +34,25 @@ var arrayShuffleQuestions;
 var questionIndex = 0;
 
 // List of Questions for Code Quiz
-var questions = 
+var questions = [
+    {q: 'Commonly used data types DO NOT include:',
+     a: '3. Alerts',
+        choices:[{choice: '1. Strings'}, {choice: '2. Booleans'}, {choice: '3. Alerts'}, {choice: '4. Numbers'}]
+    },
+    {q: 'The condition in an if / else statement is enclosed with __________.',
+     a: '3. Parenthesis',
+            choices:[{choice: '1. Quotes'}, {choice: '2. Curly Brackets'}, {choice: '3. Parenthesis'}, {choice: '4. Square Brackets'}]
+    },
+    {q: 'Arrays in Javascript can be used to store __________.',
+     a: '4. All of the above',
+        choices:[{choice: '1. Numbers'}, {choice: '2. Booleans'}, {choice: '3. Strings'}, {choice: '4. All of the above'}]
+    },
+    {q: 'String values must be enclosed within __________ when being assigned to variables.',
+     a: '3. Quotes',
+     choices:[{choice: '1. Commas'}, {choice: '2. Curly Brackets'}, {choice: '3. Quotes'}, {choice: '4. Parenthesis'}]
+    },
+    {q: 'A very useful tool used during development and debugging for printing content to the debugger is:',
+     a: '4. Console.log',
+     choices:[{choice: '1. JavaScript'}, {choice: '2. Terminal/Bash'}, {choice: '3. For Loops'}, {choice: '4. Console.log'}]
+    },
+];
